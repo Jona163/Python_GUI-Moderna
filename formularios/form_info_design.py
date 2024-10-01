93af6ca3880500a8c65e74647286600191d4636a
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-30 20:05:50 -0600

    Update form_info_design.py

--- formularios/form_info_design.py
+++ formularios/form_info_design.py
@@ -2,3 +2,21 @@
 # Fecha: 30 Septiembre 2024
 # Descripción: Código para GUI MODERNA PYTHON.
 # GitHub: https://github.com/Jona163
+
+import tkinter as tk
+from typing_extensions import Literal
+import util.util_ventana as util_ventana
+
+class FormularioInfoDesign(tk.Toplevel):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.config_window()
+        self.contruirWidget()
+
+    def config_window(self):
+        # Configuración inicial de la ventana
+        self.title('Python GUI')
+        self.iconbitmap("./imagenes/logo.ico")
+        w, h = 400, 100        
+        util_ventana.centrar_ventana(self, w, h)     
