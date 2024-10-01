ea946bdbac92bfc46430f681a415dc97eeb532fe
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-30 20:01:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,8 @@
 # Fecha: 30 Septiembre 2024
 # Descripción: Código para GUI MODERNA PYTHON.
 # GitHub: https://github.com/Jona163
+
+from formularios.form_maestro_design import FormularioMaestroDesign
+
+app = FormularioMaestroDesign()
+app.mainloop()
