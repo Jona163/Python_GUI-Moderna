f13ef906d14696c5dac0c6506dc43cd8cdc54b3d
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-30 20:07:44 -0600

    Update form_maestro_desing.py

--- formularios/form_maestro_desing.py
+++ formularios/form_maestro_desing.py
@@ -2,3 +2,61 @@
 # Fecha: 30 Septiembre 2024
 # Descripción: Código para GUI MODERNA PYTHON.
 # GitHub: https://github.com/Jona163
+
+import tkinter as tk
+from tkinter import font
+from config import COLOR_BARRA_SUPERIOR, COLOR_MENU_LATERAL, COLOR_CUERPO_PRINCIPAL, COLOR_MENU_CURSOR_ENCIMA
+import util.util_ventana as util_ventana
+import util.util_imagenes as util_img
+# Nuevo
+from formularios.form_graficas_design import FormularioGraficasDesign
+from formularios.form_sitio_construccion import FormularioSitioConstruccionDesign
+from formularios.form_info_design import FormularioInfoDesign
+
+class FormularioMaestroDesign(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+        self.logo = util_img.leer_imagen("./imagenes/logo.png", (560, 136))
+        self.perfil = util_img.leer_imagen("./imagenes/Perfil.png", (100, 100))
+        self.img_sitio_construccion = util_img.leer_imagen("./imagenes/sitio_construccion.png", (200, 200))
+        self.config_window()
+        self.paneles()
+        self.controles_barra_superior()        
+        self.controles_menu_lateral()
+        self.controles_cuerpo()
+    
+    def config_window(self):
+        # Configuración inicial de la ventana
+        self.title('Python GUI')
+        self.iconbitmap("./imagenes/logo.ico")
+        w, h = 1024, 600        
+        util_ventana.centrar_ventana(self, w, h)        
+
+    def paneles(self):        
+         # Crear paneles: barra superior, menú lateral y cuerpo principal
+        self.barra_superior = tk.Frame(
+            self, bg=COLOR_BARRA_SUPERIOR, height=50)
+        self.barra_superior.pack(side=tk.TOP, fill='both')      
+
+        self.menu_lateral = tk.Frame(self, bg=COLOR_MENU_LATERAL, width=150)
+        self.menu_lateral.pack(side=tk.LEFT, fill='both', expand=False) 
+        
+        self.cuerpo_principal = tk.Frame(
+            self, bg=COLOR_CUERPO_PRINCIPAL)
+        self.cuerpo_principal.pack(side=tk.RIGHT, fill='both', expand=True)
+    
+    def controles_barra_superior(self):
+        # Configuración de la barra superior
+        font_awesome = font.Font(family='FontAwesome', size=12)
+
+        # Etiqueta de título
+        self.labelTitulo = tk.Label(self.barra_superior, text="Autodidacta")
+        self.labelTitulo.config(fg="#fff", font=(
+            "Roboto", 15), bg=COLOR_BARRA_SUPERIOR, pady=10, width=16)
+        self.labelTitulo.pack(side=tk.LEFT)
+
+        # Botón del menú lateral
+        self.buttonMenuLateral = tk.Button(self.barra_superior, text="\uf0c9", font=font_awesome,
+                                           command=self.toggle_panel, bd=0, bg=COLOR_BARRA_SUPERIOR, fg="white")
+        self.buttonMenuLateral.pack(side=tk.LEFT)
