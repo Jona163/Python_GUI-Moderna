137e3dd7c594b96c36ee5f82820b4097657d17ec
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-30 20:01:55 -0600

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,8 @@
 # Fecha: 30 Septiembre 2024
 # Descripción: Código para GUI MODERNA PYTHON.
 # GitHub: https://github.com/Jona163
+
+COLOR_BARRA_SUPERIOR = "#1f2329"
+COLOR_MENU_LATERAL = "#2a3138"
+COLOR_CUERPO_PRINCIPAL = "#f1faff"
+COLOR_MENU_CURSOR_ENCIMA = "#2f88c5"
